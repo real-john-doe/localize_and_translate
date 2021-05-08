e6329fb03991243fef1db222202cf6bd770da55c
Reza <amini.rezza@gmail.com>
2021-05-08 20:24:51 +0430

    removed google translate api, fixed asset load exception, removed unsued dependencies

--- lib/localize_and_translate.dart
+++ lib/localize_and_translate.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'src/main.dart';
 
-enum LocalizationDefaultType { device, asDefined }
 LocalizeAndTranslate translator = new LocalizeAndTranslate();
 
 class LocalizedApp extends StatefulWidget {
--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'dart:ui';
 
+import 'package:devicelocale/devicelocale.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../localize_and_translate.dart';
@@ -14,10 +14,8 @@ class LocalizeAndTranslate {
   ///------------------------------------------------
   /// Config
   ///------------------------------------------------
-  LocalizationDefaultType? _localeType;
   List<String> _langList = [];
   String? _assetsDir;
-  String _apiKeyGoogle = '';
   Locale? _locale;
   Map<String, dynamic>? _values;
   SharedPreferences? _prefs;
@@ -26,29 +24,32 @@ class LocalizeAndTranslate {
   /// Initialize Plugin
   ///------------------------------------------------
   Future<Null> init({
-    LocalizationDefaultType? localeDefault,
-    String? language,
     required List<String> languagesList,
     String? assetsDirectory,
     Map<String, String>? valuesAsMap, // Later
-    String? apiKeyGoogle,
     SharedPreferences? prefsInstance,
   }) async {
     // ---- Vars ---- //
-    _assetsDir = assetsDirectory!.endsWith('/') ? '$assetsDirectory' : '$assetsDirectory/';
+    _assetsDir = assetsDirectory!.endsWith('/')
+        ? '$assetsDirectory'
+        : '$assetsDirectory/';
     _prefs = prefsInstance ?? await SharedPreferences.getInstance();
-    _localeType = localeDefault ?? LocalizationDefaultType.device;
     _langList = languagesList;
-    _apiKeyGoogle = apiKeyGoogle ?? '';
-    _locale = language != null
-        ? Locale(language)
-        : _prefs!.getString('currentLang') != null
-            ? Locale(_prefs!.getString('currentLang')!, "")
-            : _localeType == LocalizationDefaultType.device
-                ? '${window.locale}'.contains(RegExp('[-_]'))
-                    ? Locale('${window.locale}'.split(RegExp('[-_]'))[0])
-                    : Locale('${window.locale}')
-                : Locale(_langList[0]);
+
+    if (_prefs!.getString('currentLang') != null) {
+      _locale = Locale(_prefs!.getString('currentLang')!);
+    } else {
+      Locale? locale = await Devicelocale.currentAsLocale;
+      if (locale != null) {
+        if (_langList.contains(locale.languageCode)) {
+          _locale = locale;
+        } else {
+          _locale = Locale(_langList[0]);
+        }
+      } else {
+        _locale = Locale(_langList[0]);
+      }
+    }
 
     if (_assetsDir == null && valuesAsMap == null) {
       assert(
@@ -65,10 +66,6 @@ class LocalizeAndTranslate {
       _values = valuesAsMap;
     }
 
-    print(
-      '--LocalizeAndTranslate : Google(${apiKeyGoogle != null}) | LangList$_langList | Dir($_assetsDir) | Active(${_locale!.languageCode}.json)',
-    );
-
     return null;
   }
 
@@ -76,7 +73,7 @@ class LocalizeAndTranslate {
   /// Initialize Active Language Values
   ///------------------------------------------------
   initLanguage(String languageCode) async {
-    String filePath = "$_assetsDir$languageCode.json";
+    String filePath = '$_assetsDir$languageCode.json';
     String content = await rootBundle.loadString(filePath);
     return json.decode(content);
   }
@@ -85,58 +82,13 @@ class LocalizeAndTranslate {
   /// Transle : [key]
   ///------------------------------------------------
   String translate(String key, [Map<String, String>? arguments]) {
-    String value = (_values == null || _values![key] == null) ? '$key' : _values![key];
+    String value =
+        (_values == null || _values![key] == null) ? '$key' : _values![key];
     if (arguments == null) return value;
     for (var key in arguments.keys) {
       value = value.replaceAll(key, arguments[key]!);
     }
     return value;
-  }
-
-  ///------------------------------------------------
-  /// Google Translate
-  /// --- will be removed, if you are using it feel free to notify package admin
-  ///------------------------------------------------
-  @deprecated
-  Future<String?> translateWithGoogle({
-    required String key,
-    required String from,
-    String? to,
-  }) async {
-    try {
-      if (_apiKeyGoogle.isEmpty) {
-        print('--LocalizeAndTranslate : Google(false)');
-        return key;
-      }
-
-      if (to == null) to = _locale!.languageCode;
-
-      var response = await http.post(
-        Uri(path: 'https://translation.googleapis.com/language/translate/v2'),
-        headers: {
-          'Authorization': 'Bearer $_apiKeyGoogle',
-        },
-        body: {
-          "q": key,
-          "source": from,
-          "target": to,
-          "format": "text",
-        },
-      ).timeout(Duration(seconds: 10));
-
-      var data = json.decode(response.body);
-
-      String? text = key;
-      if (response.statusCode == 200) {
-        text = data['data']['translations']['translatedText'];
-      } else {
-        print('--LocalizeAndTranslate : $data');
-      }
-
-      return text;
-    } catch (e) {
-      return key;
-    }
   }
 
   ///------------------------------------------------
@@ -148,13 +100,13 @@ class LocalizeAndTranslate {
     bool restart = true,
     bool remember = true,
   }) async {
-    if (newLanguage == "") {
-      newLanguage = _locale?.languageCode ?? _langList[0];
+    String updatedLanguage = newLanguage;
+    if (newLanguage == '') {
+      updatedLanguage = _locale?.languageCode ?? _langList[0];
     }
+    _locale = Locale(updatedLanguage, '');
 
-    _locale = Locale(newLanguage, "");
-
-    String filePath = "$_assetsDir$newLanguage.json";
+    String filePath = '$_assetsDir$newLanguage.json';
     String content = await rootBundle.loadString(filePath);
 
     _values = json.decode(content);
@@ -174,7 +126,8 @@ class LocalizeAndTranslate {
   ///------------------------------------------------
   /// Determine Active Layout (bool)
   ///------------------------------------------------
-  isDirectionRTL(BuildContext context) => Directionality.of(context) == TextDirection.rtl;
+  isDirectionRTL(BuildContext context) =>
+      Directionality.of(context) == TextDirection.rtl;
 
   ///------------------------------------------------
   /// Restart App
@@ -204,5 +157,6 @@ class LocalizeAndTranslate {
   ///------------------------------------------------
   /// Locals List
   ///------------------------------------------------
-  Iterable<Locale> locals() => _langList.map<Locale>((lang) => new Locale(lang, ''));
+  Iterable<Locale> locals() =>
+      _langList.map<Locale>((lang) => new Locale(lang, ''));
 }
